d53a7b8a1c88eaf33e3ad7a2b9062699c5969344
zainuldeen <78583049+Zain-ul-din@users.noreply.github.com>
2024-04-19 01:13:04 +0500

    style: prettier ✨

--- apps/crawler/src/main.ts
+++ apps/crawler/src/main.ts
@@ -6,10 +6,10 @@ import { router } from './routes.js';
 const startUrls = ['https://crawlee.dev'];
 
 const crawler = new CheerioCrawler({
-    // proxyConfiguration: new ProxyConfiguration({ proxyUrls: ['...'] }),
-    requestHandler: router,
-    // Comment this option to scrape the full website.
-    maxRequestsPerCrawl: 20,
+  // proxyConfiguration: new ProxyConfiguration({ proxyUrls: ['...'] }),
+  requestHandler: router,
+  // Comment this option to scrape the full website.
+  maxRequestsPerCrawl: 20,
 });
 
 await crawler.run(startUrls);
