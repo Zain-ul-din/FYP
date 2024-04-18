71cda98a052f10a6211602a3d70b1c71102cfc57
zainuldeen <78583049+Zain-ul-din@users.noreply.github.com>
2024-04-19 01:39:41 +0500

    feat: added .env for admin sdk (#2)

--- apps/web/lib/firebase/admin-sdk.ts
+++ apps/web/lib/firebase/admin-sdk.ts
@@ -1,5 +1,18 @@
 import { initializeApp, getApps, cert, ServiceAccount } from 'firebase-admin/app';
-import adminCredentials from './firebase-admin-credentials.json';
+
+const adminCredentials = {
+  type: process.env.TYPE || '',
+  projectId: process.env.PROJECT_ID || '',
+  privateKeyId: process.env.PRIVATE_KEY_ID || '',
+  privateKey: process.env.PRIVATE_KEY || '',
+  clientEmail: process.env.CLIENT_EMAIL || '',
+  clientId: process.env.CLIENT_ID || '',
+  authUri: process.env.AUTH_URI || '',
+  tokenUri: process.env.TOKEN_URI || '',
+  authProviderX509CertUrl: process.env.AUTH_PROVIDER_X509_CERT_URL || '',
+  clientX509CertUrl: process.env.CLIENT_X509_CERT_URL || '',
+  universeDomain: process.env.UNIVERSE_DOMAIN || '',
+};
 
 const firebaseAdminConfig = {
   credential: cert(adminCredentials as ServiceAccount),
